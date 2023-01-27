6c28e974a2c48779f6b3bf49ffccb5b7cd67d9e5
victo <victo@LAPTOP-HMNM0KE1>
2023-01-27 12:27:23 -0500

    added student class

---
+++ src/Student.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author victo
+ */
+public class Student {
+    private String studentName;
+    private String address;
+    
+    public Student (String studentName, String studentId ){
+        
+    }
+}
