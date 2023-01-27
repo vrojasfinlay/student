1a54153eb8dddc0763c0593351ff140c806aa2d9
victo <victo@LAPTOP-HMNM0KE1>
2023-01-27 11:39:30 -0500

    added more modified comments

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -9,6 +9,7 @@ public class JavaApplication2 {
     /**
      * @param args the command line arguments
      * modified at 11:32 Friday 27,2023
+     * added comments at 11:38
      */
     public static void main(String[] args) {
         
