a151b51b1ef7345e59021d92bc8306ecd6802879
victo <victo@LAPTOP-HMNM0KE1>
2023-01-27 11:40:35 -0500

    remove the added comments

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -9,7 +9,6 @@ public class JavaApplication2 {
     /**
      * @param args the command line arguments
      * modified at 11:32 Friday 27,2023
-     * added comments at 11:38
      */
     public static void main(String[] args) {
         
