8f1f8e33e39c78b9971718297c7b97c7e3633bb2
victo <victo@LAPTOP-HMNM0KE1>
2023-01-27 11:36:19 -0500

    added modified commit

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -8,6 +8,7 @@ public class JavaApplication2 {
 
     /**
      * @param args the command line arguments
+     * modified at 11:32 Friday 27,2023
      */
     public static void main(String[] args) {
         
